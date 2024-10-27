14e31df5e282ad659653328212909e8876853433
learnpythonfree <freezo12191@gmail.com>
2024-10-27 14:51:48 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Specify the paths to all of the template files
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
